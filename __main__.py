62f37aa2ebf94f7446ab928ad6f49675a8034cd8
OMAR TOUIL <touil.omar44@gmail.com>
2024-04-05 15:25:46 +0200

    UPDATE FOR LOGIN/SIGN UP

--- __main__.py
+++ __main__.py
@@ -12,5 +12,14 @@ def hello():
 def About():
     return render_template("about.html")
 
+
+@app.route("/MakeAppointment")
+def MakeAppointment():
+  return render_template("makeAppointment.html")
+
+@app.route("/Lang/<lang>")
+def Lang(lang):
+    return "Chamgnemt du langue"+ lang
+
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0', port=5000)
